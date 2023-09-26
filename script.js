91ac75ff482eee6a51f284479d5f6246bb23a769
MohcineDev <mouhcinedev@gmail.com>
2023-09-26 01:33:50 +0100

    add recent files dialog box

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const uploadFile = document.querySelector('.upload')
 const uploadInput = document.querySelector('#upload_input')
 const recentFilesMoreOption = document.querySelectorAll('.row svg:last-of-type')
-const moreOptionRecentFiles  = document.querySelector('.more_option_recent_files')
+const recentFilesDialog = document.querySelector('.more_option_recent_files')
 
-uploadFile.onclick =()=>{
+uploadFile.onclick = () => {
 	uploadInput.click()
 }
 
 recentFilesMoreOption.forEach(svg => {
+
+	let width = recentFilesDialog.offsetWidth
+	let height = recentFilesDialog.offsetHeight
+	//initialize the values
+	recentFilesDialog.style.left = 0;
+	recentFilesDialog.style.top = 0
+
 	svg.onclick = (e) => {
-		console.log(moreOptionRecentFiles)
-		console.log(moreOptionRecentFiles.offsetWidth)
-		moreOptionRecentFiles.style.left = `${ e.x - moreOptionRecentFiles.offsetWidth}px`
-		moreOptionRecentFiles.style.top = `${e.y - moreOptionRecentFiles.offsetWidth}px`
-moreOptionRecentFiles.style.display="block";
+
+		console.log(e.x, e.y)
+
+		recentFilesDialog.style.left = `${e.x - width}px`
+		recentFilesDialog.style.top = `${e.y - height}px`
+		recentFilesDialog.style.display = "block";
+
+		document.body.addEventListener('click', (e) => {
+			if (e.target.classList[0] != 'more_option_recent_files' && e.target.tagName != 'svg') {
+				recentFilesDialog.style.display = "none"
+			}  
+		})
 	}
 
 })
